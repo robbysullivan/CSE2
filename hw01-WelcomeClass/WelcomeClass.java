e31f0a6fe872f04a33475bb52c2ffcfd93430b4b
robbysullivan <robbysullivan1313@comcast.net>
2014-08-31 17:48:30 +0000

    updated WelcomeClass

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,29 @@
+//  Robert Sullivan
+//  hw01
+//  8/31/14
+
+//  first complile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+public static void main(String[] args) {
+//  print the statement 
+System.out.println("-----------");
+System.out.println("| WELCOME |");
+System.out.println("-----------");
+System.out.println("  ^  ^  ^  ^  ^  ^  ");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println("|-R--J--S--2--1--7-|");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+System.out.println("  v  v  v  v  v  v  ");
+System.out.println("This summer my family and I took a two");
+System.out.println("week vacation to Italy. The food was great.");
+
+}
+}
+
