3cb919e8aa0806e691e8b77db0fbcd33cdc2fc4e
robbysullivan <robbysullivan1313@comcast.net>
2014-09-22 00:17:05 +0000

    Updated Month

---
+++ hw04/Month.java
@@ -0,0 +1,48 @@
+//  Robert Sullivan
+//  CSE2
+//  9/21/14
+
+//  find the number of days in a month
+//  import scanner
+import java.util.Scanner;
+
+//  define a class
+public class Month{
+//  create main method
+public static void main(String[] args){
+    Scanner myScanner;
+    myScanner = new Scanner(System.in);
+//  enter month
+System.out.print("Enter month of year (1-12): ");
+//  if statement
+    if(myScanner.hasNextInt()){
+        int month = myScanner.nextInt();
+    //  next if statement
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            System.out.println("The month has 31 days.");
+        }// end if
+        else if(month == 4 || month == 6 || month == 9 || month == 11){
+            System.out.println("The month has 30 days.");
+        }// end else if
+        else if(month == 2){
+            System.out.print("Enter an int giving the year: ");
+            double year = myScanner.nextInt();
+            year = (double) year*0.25;
+            if(year == 0){
+                System.out.println("The month has 29 days.");
+            }// end if
+            else{
+                System.out.println("The month has 28 days.");
+            }// end else
+        }// end else if
+        else{
+            System.out.println("The number was not 1-12.");
+            return;
+        }// end else
+    }// end if
+    else{
+        System.out.println("You did not enter an integer!");
+        return;
+    }// end else
+}// end main method
+}// end class
