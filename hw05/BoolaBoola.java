e0f8a6e9bd2e432f65762b009d9be4974af5bb8b
robbysullivan <robbysullivan1313@comcast.net>
2014-10-01 23:51:45 +0000

    Updated BurgerKing

---
+++ hw05/BoolaBoola.java
@@ -0,0 +1,23 @@
+//  Robert Sullivan 
+//  CSE2
+//  9/29/14
+
+//  randomize true and false statments
+
+//  compile the program
+//      javac BolaBoola.java
+//  run the program
+//      java BoolaBoola
+
+//  import scanner
+import java.util.Scanner;
+
+//  define a class
+public class BoolaBoola{
+//  create main method
+public static void main(String[] args){
+    Scanner myScanner;
+    myScanner = new Scanner(System.in);
+
+}// end main method
+}// end class
