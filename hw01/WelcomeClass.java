8e066171feba5f3fdab7c2c1974a4f0671e644c7
robbysullivan <robbysullivan1313@comcast.net>
2014-11-18 22:18:05 +0000

    Updated FindDuplicates

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -1,29 +1,30 @@
-//  Robert Sullivan
-//  hw01
-//  8/31/14
-
-//  first complile the program
-//      javac WelcomeClass.java
-//  run the program
-//      java WelcomeClass//
-
-//  define a class
 public class WelcomeClass{
-
-//  add main method
-public static void main(String[] args) {
-//  print the statement 
-System.out.println("-----------");
-System.out.println("| WELCOME |");
-System.out.println("-----------");
-System.out.println("  ^  ^  ^  ^  ^  ^  ");
-System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
-System.out.println("|-R--J--S--2--1--7-|");
-System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
-System.out.println("  v  v  v  v  v  v  ");
-System.out.println("This summer my family and I took a two");
-System.out.println("week vacation to Italy. The food was great.");
+    
+public static void main(String[] args){
+int j = 1;
+int counter = 1;
+for(int i=1;i<=8;i++){
+    
+    if(i%j == 0){
+        
+        for(int k=1;k<=counter;k++){
+           for(int l=1;l<=k;l++){
+               if(k==l){
+                   System.out.println(i);
+               }
+               else{
+                   System.out.print(i);
+               }
+           } 
+        }
+    
+        j = i;
+        
+        counter++;
+    }
+    else{continue;}
+}
 
 }
-}
 
+}
